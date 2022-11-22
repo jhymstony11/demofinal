8116e6cf492b3fbed1e951413d527b24fbc3a718
jhymstony11 <118759772+jhymstony11@users.noreply.github.com>
2022-11-22 12:41:22 -0500

    Update sesion13.js

--- js/sesion13.js
+++ js/sesion13.js
@@ -1,70 +1,70 @@
 // EJERCIO  1
-  // let num1 = parseInt(prompt("Ingrese el primer numero:"));
-  // let num2 = parseInt(prompt("Ingrese el segundo numero:"));
-  // let resultado = num1 + num2;
-  // let resultado2 = num1 * num2;
-  // alert(`La suma es de ${resultado} y la multipicacion es de ${resultado2}`);
+   let num1 = parseInt(prompt("Ingrese el primer numero:"));
+   let num2 = parseInt(prompt("Ingrese el segundo numero:"));
+   let resultado = num1 + num2;
+   let resultado2 = num1 * num2;
+   alert(`La suma es de ${resultado} y la multipicacion es de ${resultado2}`);
 
 //EJERCIO  2
-//  let num3 = parseInt(prompt("Ingrese un numero:"));
-//  let num4 = num3 * 0.2;
-//  alert(`El 20% del numero es ${num4}`);
+  let num3 = parseInt(prompt("Ingrese un numero:"));
+  let num4 = num3 * 0.2;
+  alert(`El 20% del numero es ${num4}`);
 
 //EJERCICIO 3
 
-// let numero5 = 0;
-// numero5 = prompt("Ingrese numero en metros: ");
-// let convertircm = parseInt(numero5) * 100;
-// let convertirmm = parseInt(numero5) * 1000;
-// alert(`Equivale a ${convertircm} cm y ${convertircm} mm`);
+ let numero5 = 0;
+ numero5 = prompt("Ingrese numero en metros: ");
+ let convertircm = parseInt(numero5) * 100;
+ let convertirmm = parseInt(numero5) * 1000;
+ alert(`Equivale a ${convertircm} cm y ${convertircm} mm`);
 
 // EJERCICIO 4
 
-// let base = 0;
-// let altura = 0;
-// base=prompt("ingrese el valor de la base:");
-// altura=prompt("ingrese el valor de la altura:");
-// let area = parseInt(base*altura/2);
-// alert(`El area del triangulo es: ${area} unidades`); 
+ let base = 0;
+ let altura = 0;
+ base=prompt("ingrese el valor de la base:");
+ altura=prompt("ingrese el valor de la altura:");
+ let area = parseInt(base*altura/2);
+ alert(`El area del triangulo es: ${area} unidades`); 
 
 // EJERCICIO 5
 
-// let dolares = 0;
-// dolares = prompt("Ingrese monto en dolares: ");
-// let convertir = parseInt(dolares) * 3.38;
-// alert(`Equivale a ${convertir} soles`);
+ let dolares = 0;
+ dolares = prompt("Ingrese monto en dolares: ");
+ let convertir = parseInt(dolares) * 3.38;
+ alert(`Equivale a ${convertir} soles`);
 
 
 // EJERCICIO 6
 
-// medida = parseInt(prompt("Ingrese el tamaño en pulgadas:"));
-// milimetro = medida * 25.4;
-// alert(`Equivale a ${milimetro} milimetros`);
+ medida = parseInt(prompt("Ingrese el tamaño en pulgadas:"));
+ milimetro = medida * 25.4;
+ alert(`Equivale a ${milimetro} milimetros`);
 
 
 // EJERCICIO 7
 
-// numero = prompt("Ingrese el numero aqui:");
-// x = parseInt(numero[0]);
-// y = parseInt(numero[1]);
-// z = parseInt(numero[2]);
-// resultado = (x + y + z) **2 ;
-// alert(`Sus cifras sumadas elevada al cuadrado equivale a ${resultado}`);
+ numero = prompt("Ingrese el numero aqui:");
+ x = parseInt(numero[0]);
+ y = parseInt(numero[1]);
+ z = parseInt(numero[2]);
+ resultado = (x + y + z) **2 ;
+ alert(`Sus cifras sumadas elevada al cuadrado equivale a ${resultado}`);
 
 // EJERCICIO 8
 
-// monto = parseInt(prompt("Ingrese el monto de dinero:"));
-// porcentaje1 = monto * 0.3;
-// porcentaje2 = monto * 0.2;
-// porcentaje3 = monto * 0.5;
-// alert(`Los porcentajes que le tocara a cada uno son ${porcentaje1} , ${porcentaje2} , ${porcentaje3}`);
+ monto = parseInt(prompt("Ingrese el monto de dinero:"));
+ porcentaje1 = monto * 0.3;
+ porcentaje2 = monto * 0.2;
+ porcentaje3 = monto * 0.5;
+ alert(`Los porcentajes que le tocara a cada uno son ${porcentaje1} , ${porcentaje2} , ${porcentaje3}`);
 
 // EJERCICIO 9
 
-// producto = parseInt(prompt("Ingrese el precio del producto:"));
-// igv = producto * 0.19;
-// preciofinal =igv + producto;
-// alert(`El IGV es ${igv} y el presio final es ${preciofinal}`);
+ producto = parseInt(prompt("Ingrese el precio del producto:"));
+ igv = producto * 0.19;
+ preciofinal =igv + producto;
+ alert(`El IGV es ${igv} y el presio final es ${preciofinal}`);
 
 // EJERCICIO 10
 
